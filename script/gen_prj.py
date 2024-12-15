99f62a43eac8e3bdf1efafec0597efb382368e82
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-15 16:07:33 +0900

    project generator improve, compile option flag for target selecting added. and  I2C test sample updated

--- script/gen_prj.py
+++ script/gen_prj.py
@@ -6,6 +6,10 @@ import	subprocess
 source_folder_name	= "source_files"
 target_boards		= [ "FRDM_MCXA153", "FRDM_MCXA156", "FRDM_MCXN236", "FRDM_MCXN947" ]
 
+library_folders		= [ "_r01lib_frdm_mcxa153", "_r01lib_frdm_mcxa156", "_r01lib_frdm_mcxn236", "_r01lib_frdm_mcxn947" ]
+template_folders	= []
+app_folders			= []
+
 
 app_template_prj	= "app_template_"
 build_configs		= [ "Debug", "Release" ]
@@ -27,18 +31,27 @@ print( "project sources:\n    " + "\n    ".join( prj_name ) )
 for t in target_boards:
 	template	= app_template_prj + t
 	print( "using template : " + template )
+	template_folders	+= [ template ]
 	
 	for p in prj_name:
 		commands	= []
 		new_prj	= p + "_" + t
 		
 		print( "    generating project: " + new_prj )
-
-		commands	+= [ "cp -R " + template + " " + new_prj ]	#	copy template
-		commands	+= [ "cp -R " + source_folder_name + "/" + p + "/ " + new_prj + "/source/" ]	#	copy source files
+		app_folders	+= [ new_prj ]
+		
+		commands	+= [ "cp -r " + template + "/ " + new_prj + "/" ]	#	copy template
+		commands	+= [ "cp -r " + source_folder_name + "/" + p + "/ " + new_prj + "/source/" ]	#	copy source files
 		commands	+= [ "rm -rf " + " ".join( [ new_prj + "/" + i + "/" for i in build_configs ] ) ]		#	delete built folders
 		commands	+= [ "sed -i -e s/'<name>" + template + "'/'<name>" + new_prj + "'/ " + new_prj + "/.project" ]
 		
 		for c in commands:
-			#print( "    executing command: " + c )
+			print( "    executing command: " + c )
 			subprocess.run( c, shell = True )
+
+commands	= []
+commands	= [ "zip -r apps_for_importing.zip " + " ".join( library_folders ) + " " + " ".join( template_folders ) + " " + " ".join( app_folders ) ]
+
+for c in commands:
+	print( "    executing command: " + c )
+	subprocess.run( c, shell = True )
