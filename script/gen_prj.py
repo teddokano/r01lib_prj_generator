a3890de42f37573406bcee3dd810f341bdd19f3c
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-16 05:36:49 +0900

    generator script generated a zip file with folder name

--- script/gen_prj.py
+++ script/gen_prj.py
@@ -4,7 +4,6 @@ import	os
 import	subprocess
 import	argparse
 
-source_folder_name	= "source_files"
 suppported_boards	= [ "FRDM_MCXA153", "FRDM_MCXA156", "FRDM_MCXN236", "FRDM_MCXN947" ]
 
 exec				= False
@@ -13,37 +12,31 @@ library_folders		= [ "_r01lib_frdm_mcxa153", "_r01lib_frdm_mcxa156", "_r01lib_fr
 app_template_prj	= "app_template_"
 build_configs		= [ "Debug", "Release" ]
 
-default_zip_name	= "apps_for_importing"
-
 def main():
-	if args.input_folder:
-		source_folder_name	= args.input_folder
+	base_dir	= os.getcwd() + "/"
 
-	if args.zipfile:
-		output_zip_name		= args.zipfile
-	else:
-		output_zip_name		= default_zip_name + ".zip"
+	source_folder_path	= os.path.dirname( args.input_folder )
+	source_folder_name	= os.path.basename( source_folder_path )
 
 	if args.target:
 		args.target		= args.target.strip( "'" )
 		target_boards	= []
 		for n in suppported_boards:
-			print( args.target + "<>" + n )
 			if args.target in n:
 				target_boards	+= [ n ]
 	else:
 		target_boards	= suppported_boards
-		
-	base_dir	= os.getcwd() + "/"
 
-	source_path	= [ i.path for i in os.scandir( base_dir + "/" + source_folder_name )  if "/." not in i.path ]
+	if args.zipfile:
+		output_zip_name		= args.zipfile
+	else:
+		output_zip_name		= source_folder_name + "_" + "_".join( target_boards ) + ".zip"
+
+	source_path	= [ i.path for i in os.scandir( base_dir + "/" + source_folder_path )  if "/." not in i.path ]
 	source_path	= [ i for i in source_path if "/." not in i ]
 	source_path.sort()
 
-	print( base_dir )
-
 	prj_name	= [ os.path.basename( i ) for i in source_path ]
-	#prj_folders	= [ p + "_" + t for p in prj_name for t in target_boards ]
 
 	print( "======= process started for .. =======" )
 	print( "project sources:\n    " + "\n    ".join( prj_name ) )
@@ -71,8 +64,10 @@ def main():
 			for c in commands:
 				print( "    executing command: " + c )
 				
-				if not args.no_exec:
+				if not args.no_exec and not args.delete:
 					subprocess.run( c, shell = True )
+
+	### zipping
 
 	commands	 = []
 	commands	+= [ "rm -rf " + output_zip_name ]
@@ -84,25 +79,35 @@ def main():
 		if not args.no_exec:
 			subprocess.run( c, shell = True )
 
+	### deleting projects after zipping
+
+	if not args.keep:
+		commands	 = []
+		commands	+= [ "rm -rf " + " ".join( app_folders ) ]
+
+		for c in commands:
+			print( "    executing command: " + c )
+
+			if not args.no_exec:
+				subprocess.run( c, shell = True )
+
 
 def command_line_handling():
 	parser	= argparse.ArgumentParser( description = "r01lib MCUXpresso project generator" )
 	qv_grp	= parser.add_mutually_exclusive_group()
 	
 	parser.add_argument( "input_folder",			help = "input source folder name" )
-	parser.add_argument( "-t", "--target",			help = "target board", 			type=ascii )
-	parser.add_argument( "-z", "--zipfile",			help = "output zip file name", 	type=ascii )
-	qv_grp.add_argument( "-N", "--no_exec", 		help = "no execution",			action = "store_true" )
+	parser.add_argument( "-t", "--target",			help = "target board", 				type=ascii )
+	parser.add_argument( "-z", "--zipfile",			help = "output zip file name", 		type=ascii )
+	qv_grp.add_argument( "-d", "--delete", 			help = "delete project folders",	action = "store_true" )
+	qv_grp.add_argument( "-k", "--keep", 			help = "keep project folders",		action = "store_true" )
+	qv_grp.add_argument( "-N", "--no_exec", 		help = "no execution",				action = "store_true" )
 	
 	return	parser.parse_args()
 
 if __name__ == "__main__":
 	args	= command_line_handling()
 	
-	print( args.input_folder )
-	print( args.target )
-	print( args.zipfile )
-	print( args.no_exec )
 	if args.no_exec:
 		exec	= False
 	main()
