4edeeef686120e57335acd6a4217bd4188d10412
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-18 11:14:21 +0900

    GPIO interrupt app:correcting # of ports on screen

--- source_files/arduino_shields/GPIO_PCAL6416AEV-ARD_3_Interrupt/main.cpp
+++ source_files/arduino_shields/GPIO_PCAL6416AEV-ARD_3_Interrupt/main.cpp
@@ -56,7 +56,7 @@ int main(void)
 
 			gpio.read_port( INT_STATUS, status );
 
-			printf( "[INT] status 0~1:" );
+			printf( "[INT] status 0~%d:", gpio.n_ports - 1 );
 
 			for ( int i = 0; i < gpio.n_ports; i++ )
 				GPIO_base::print_bin( status[i] );
