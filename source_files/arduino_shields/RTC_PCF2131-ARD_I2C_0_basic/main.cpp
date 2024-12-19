f53a8a183de4fa06fe58586f83e29dacf3976795
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-19 16:55:42 +0900

    PCF2131 sample code updated

---
+++ source_files/arduino_shields/RTC_PCF2131-ARD_I2C_0_basic/main.cpp
@@ -0,0 +1,95 @@
+/*
+ *  @author Tedd OKANO
+ *
+ *  Released under the MIT license License
+ */
+
+#include	"r01lib.h"
+#include	"I2C_device.h"
+#include	"rtc/PCF2131.h"
+#include	"rtc/RTC_NXP.h"
+
+#define		USE_I2C
+
+#ifdef	USE_I2C
+I2C			i2c( I2C_SDA, I2C_SCL );	//	SDA, SCL
+uint8_t 	i2c_address = (0xA6 >> 1);
+I2C_device	device( i2c, i2c_address );
+#define	INTF_STR	"I2C"
+#else
+SPI			spi( D11, D12, D13, D10 );	//	MOSI, MISO, SCLK, CS
+SPI_for_RTC	device( spi );
+#define	INTF_STR	"SPI"
+#endif
+
+void	set_time( void );
+void	show_time( void );
+uint8_t	bcd2dec( uint8_t v );
+uint8_t	dec2bcd( uint8_t v );
+
+int main( void )
+{
+	printf( "***** Hello, PCF2131! (%s interface) *****\r\n", INTF_STR );
+
+	if ( device.reg_r( PCF2131::Seconds ) & 0x80 )
+	{
+		printf( "==== oscillator_stop detected :( ====\r\n" );
+		set_time();
+	}
+	else
+	{
+		printf( "---- RTC has beeing kept running! :) ----\r\n" );
+	}
+
+	while ( true )
+	{
+		show_time();
+		wait( 1 );
+	}
+}
+
+void	show_time( void )
+{
+	static const char	*month_name[]	= {
+					"January", "February", "March", "April", "May", "June",
+					"July", "August", "September", "October", "November", "December"
+				};
+
+	uint8_t		bf[ 8 ];
+
+	device.reg_r( PCF2131::_100th_Seconds, bf, sizeof( bf ) );
+
+	for ( unsigned int i = 0; i < sizeof( bf ); i++ )
+		bf[ i ]	= bcd2dec( bf[ i ] );
+
+	printf( "%4d-%s-%2d, %02d:%02d:%02d\r\n",  bf[ 7 ] + 2000, month_name[ bf[ 6 ] - 1 ], bf[ 4 ], bf[ 3 ], bf[ 2 ], bf[ 1 ] );
+}
+
+void set_time( void ) {
+	uint8_t		bf[ 8 ];
+
+	bf[ 0 ]	= dec2bcd(  0 );			//	1/00 sec
+	bf[ 1 ]	= dec2bcd( 55 );			//	sec
+	bf[ 2 ]	= dec2bcd( 59 );			//	min
+	bf[ 3 ]	= dec2bcd( 23 );			//	hour
+	bf[ 4 ]	= dec2bcd( 20 );			//	day
+	bf[ 6 ]	= dec2bcd( 12 );			//	month (1 ~ 12)
+	bf[ 7 ]	= dec2bcd( 2024 - 2000 );	//	year
+
+	device.bit_op8( PCF2131::Control_1, ~0x28, 0x20 );
+	device.bit_op8( PCF2131::SR_Reset,  (uint8_t)(~0x80), 0x80 );
+
+	device.reg_w( PCF2131::_100th_Seconds, bf, sizeof( bf ) );
+
+	device.bit_op8( PCF2131::Control_1, ~0x20, 0x00 );
+}
+
+uint8_t	bcd2dec( uint8_t v )
+{
+	return (v >> 4) * 10 + (v & 0x0F);
+}
+
+uint8_t	dec2bcd( uint8_t v )
+{
+	return ((v / 10) << 4) + (v % 10);
+}
---
+++ source_files/arduino_shields/RTC_PCF2131-ARD_I2C_2_interrupts/main.cpp
@@ -0,0 +1,169 @@
+/*
+ *  @author Tedd OKANO
+ *
+ *  Released under the MIT license License
+ */
+
+#include	"r01lib.h"
+#include	"rtc/PCF2131.h"
+#include	"rtc/tzcode/strptime.h"
+#include	<time.h>
+
+#define		USE_I2C
+
+#ifdef	USE_I2C
+	I2C		interface( I2C_SDA, I2C_SCL );	//	SDA, SCL
+	#define	INTF_STR	"I2C"
+#else
+	SPI		interface( D11, D12, D13, D10 );	//	MOSI, MISO, SCLK, CS
+	#define	INTF_STR	"SPI"
+#endif
+
+PCF2131		rtc( interface );
+
+InterruptIn	intA( D8 );
+InterruptIn	intB( D9 );
+
+bool int_flagA	= false;
+bool int_flagB	= false;
+
+void	int_cause_monitor( uint8_t* status );
+void	pin_int_callbackA( void );
+void	pin_int_callbackB( void );
+void	set_time( void );
+
+int main( void )
+{
+	printf( "***** Hello, PCF2131! (%s interface) *****\r\n", INTF_STR );
+
+	if ( rtc.oscillator_stop() )
+	{
+		printf( "==== oscillator_stop detected :( ====\r\n" );
+		set_time();
+	}
+	else
+	{
+		printf( "---- RTC has beeing kept running! :) ----\r\n" );
+	}
+
+	rtc.int_clear();
+	intA.fall( pin_int_callbackA );
+	intB.fall( pin_int_callbackB );
+
+	rtc.timestamp( 1, PCF2131::LAST, PCF2131::INT_B );
+	rtc.timestamp( 2, PCF2131::LAST, PCF2131::INT_B );
+	rtc.timestamp( 3, PCF2131::LAST, PCF2131::INT_B );
+	rtc.timestamp( 4, PCF2131::LAST, PCF2131::INT_B );
+
+	rtc.periodic_interrupt_enable( PCF2131::EVERY_SECOND, PCF2131::INT_A );
+	rtc.alarm(PCF2131::SECOND, 37, PCF2131::INT_B );
+	
+	while ( true )
+	{
+		if ( int_flagA || int_flagB )
+		{
+			if ( int_flagA )
+			{
+				int_flagA	= false;
+				printf( "[INT-A] " );
+			}
+			if ( int_flagB )
+			{
+				int_flagB	= false;
+				printf( "[INT-B] " );
+			}
+
+			uint8_t status[3];
+			rtc.int_clear(status);
+			int_cause_monitor(status);
+		}
+	}
+}
+
+void int_cause_monitor( uint8_t* status )
+{
+	printf( "status:" );
+
+	for (int i = 0; i < 3; i++)
+		printf( " %02X", status[i] );
+
+	printf( ", " );
+
+	if ( status[0] & 0x80 )
+	{
+		time_t current_time = rtc.time(NULL);
+		printf( "INT:every min/sec, time:%lu %s", (unsigned long)current_time, ctime( &current_time ) );
+	}
+	if ( status[0] & 0x40 )
+	{
+		printf( "INT:watchdog" );
+	}
+	if ( status[0] & 0x10 )
+	{
+		printf( "INT:alarm " );
+		printf( "########## ALARM ########## \r\n" );
+	}
+	if ( status[1] & 0x08 )
+	{
+		printf( "INT:battery switch over" );
+	}
+	if ( status[1] & 0x04 )
+	{
+		printf( "INT:battery low" );
+	}
+	if ( status[2] & 0xF0 )
+	{
+		for ( int i = 0; i < 4; i++ )
+			if ( status[2] & (0x80 >> i) )
+				printf( "INT:timestamp%d\r\n", i + 1 );
+
+		for ( int i = 0; i < 4; i++ )
+		{
+			time_t ts = rtc.timestamp(i + 1);
+			printf( "  TIMESTAMP%d: %s", i + 1, ctime( &ts ) );
+		}
+	}
+}
+
+void pin_int_callbackA( void ) {
+  int_flagA	= true;
+}
+
+void pin_int_callbackB( void ) {
+  int_flagB	= true;
+}
+
+#define	TIMEZONE		+9
+#define	DATETIME_FORMAT	"%Y-%m-%d %H:%M:%S"
+
+void set_time( void ) {
+	struct tm	now_tm;
+
+#ifdef SEMIHOST_OPERATION
+
+	#define	CURRENT_TIME_INIT_MESSAGE	"from PC using \"time.h\" API via semihost"
+
+	char current_time[64];
+	time_t	t = time(NULL) + ((TIMEZONE) * 3600);
+	strftime( current_time, sizeof( current_time ), DATETIME_FORMAT, gmtime( &t ) );
+
+	gmtime_r( &t, &now_tm );
+
+#else
+
+	#define	DATETIME_FOR_INITIALIZE		"2024-12-20 23:59:30"
+	#define	DATETIME_FORMAT				"%Y-%m-%d %H:%M:%S"
+	#define	CURRENT_TIME_INIT_MESSAGE	"from \"DATETIME_FOR_INITIALIZE\" in source code since no current datetime information source available"
+
+	const char* current_time	= DATETIME_FOR_INITIALIZE;
+	const char* format			= DATETIME_FORMAT;
+
+	strptime( current_time, format, &now_tm );
+
+#endif
+
+	rtc.set( &now_tm );
+
+	printf( "RTC got new time information %s\r\n", CURRENT_TIME_INIT_MESSAGE );
+	printf( "  date and time is initialized to %s\r\n", current_time );
+}
