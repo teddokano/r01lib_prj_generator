cb6fa6d34798ff0a00eb02c3f5d386096b49e2bd
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-24 10:53:07 +0900

    document update

--- docs/r01lib_docs/search/files_b.js
+++ docs/r01lib_docs/search/files_b.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['semihost_5fhardfault_2ec_0',['semihost_hardfault.c',['../semihost__hardfault_8c.html',1,'']]],
-  ['spi_2ecpp_1',['spi.cpp',['../spi_8cpp.html',1,'']]],
-  ['spi_2eh_2',['spi.h',['../spi_8h.html',1,'']]],
-  ['spi_5ffor_5fafe_2ecpp_3',['SPI_for_AFE.cpp',['../_s_p_i__for___a_f_e_8cpp.html',1,'']]],
-  ['spi_5ffor_5fafe_2eh_4',['SPI_for_AFE.h',['../_s_p_i__for___a_f_e_8h.html',1,'']]],
-  ['spi_5ffor_5frtc_2ecpp_5',['SPI_for_RTC.cpp',['../_s_p_i__for___r_t_c_8cpp.html',1,'']]]
+  ['tempsensor_2ecpp_0',['TempSensor.cpp',['../_temp_sensor_8cpp.html',1,'']]],
+  ['tempsensor_2eh_1',['TempSensor.h',['../_temp_sensor_8h.html',1,'']]],
+  ['test_5flm75b_2ecpp_2',['test_LM75B.cpp',['../test___l_m75_b_8cpp.html',1,'']]],
+  ['test_5flm75b_2eh_3',['test_LM75B.h',['../test___l_m75_b_8h.html',1,'']]],
+  ['ticker_2ecpp_4',['Ticker.cpp',['../_ticker_8cpp.html',1,'']]],
+  ['ticker_2eh_5',['Ticker.h',['../_ticker_8h.html',1,'']]]
 ];
---
+++ docs/r01lib_docs/search/typedefs_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['raw_5ft_0',['raw_t',['../class_n_a_f_e13388___base.html#a2a5d7cd027402c2bd556ca7db2d5f473',1,'NAFE13388_Base::raw_t'],['../class_temp_sensor.html#a64dc9c3b6a13b8a3f4c7ad966fb01b8b',1,'TempSensor::raw_t']]]
+];
