546689e561ec6e705dd6b4a197e9d4e7daebf2a7
Tedd OKANO <okano.akifumi@gmail.com>
2025-01-01 13:13:06 +0900

    PCA9955B demo updated

--- source_files/mcx_handson_prototypes_iostream/1_7_ARD_PCA9955B_9LEDs/main.cpp
+++ source_files/mcx_handson_prototypes_iostream/1_7_ARD_PCA9955B_9LEDs/main.cpp
@@ -1,4 +1,9 @@
-//FILEHEAD
+/*
+ *  @author Tedd OKANO
+ *
+ *  Released under the MIT license License
+ */
+
 #include	"r01lib.h"
 #include	"led/PCA9955B.h"
 #include	<math.h>
@@ -8,10 +13,12 @@ using namespace	std;
 I2C			i2c( A4, A5 );	//	SDA, SCL
 PCA9955B	drv( i2c );
 
-constexpr int	period	= 100;
-constexpr float	pi2		= 2.0 * M_PI;
-constexpr float	amp		= 0.5;
-constexpr float	offset	= 0.5;
+constexpr int	period		= 100;
+constexpr float	pi2			= 2.0 * M_PI;
+constexpr float	amp			= 0.5;
+constexpr float	offset		= 0.5;
+constexpr int	n_colors	= 3;
+constexpr int	rgb_leds	= 3;
 
 float	sin_wave_value( float phase );
 
@@ -25,14 +32,18 @@ int main( void )
 
 	drv.begin( 1.0, PCA9955B::ARDUINO_SHIELD );
 
+	float	rgb[ 3 ];
+
 	while ( true )
 	{
 		for ( int i = 0; i < period; i++ )
 		{
-			for ( int ch = 0; ch < 9; ch++ )
-			{
-				drv.pwm( ch, sin_wave_value( (float)i / (float)period + (ch % 3) / 3.0) );
-			}			
+			for ( int color = 0; color < n_colors; color++ )
+				rgb[ color ]	= sin_wave_value( (float)i / (float)period + (color % n_colors) / (float)n_colors );
+
+			for ( int ch = 0; ch < n_colors * rgb_leds; ch++ )
+				drv.pwm( ch, rgb[ ch % 3 ] );
+
 			wait( 0.01 );
 		}
 	}
@@ -40,5 +51,5 @@ int main( void )
 
 float sin_wave_value( float phase )
 {
-	return	offset + amp * sin( pi2 * phase );
+	return offset + amp * sin( pi2 * phase );
 }
