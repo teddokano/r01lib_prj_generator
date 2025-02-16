dac7a69da69abbab523316da530191e7a4ab2489
Tedd OKANO <okano.akifumi@gmail.com>
2025-02-17 06:42:57 +0900

    "r01lib" updated (v0.35.0)

--- source_files/uim/AFE_UIM_NAFE13388_1_DRDY/main.cpp
+++ source_files/uim/AFE_UIM_NAFE13388_1_DRDY/main.cpp
@@ -1,3 +1,9 @@
+/*
+ *  @author Tedd OKANO
+ *
+ *  Released under the MIT license
+ */
+
 #include	"r01lib.h"
 #include	"afe/NAFE13388_UIM.h"
 
@@ -8,31 +14,36 @@ volatile bool	drdy_wait;
 
 constexpr uint32_t	timeout_limit	= 100000000;
 
-void	DRDY_int_handler( void );
-void	logical_ch_config_view( int channel );
-void	register16_dump( const std::vector<uint16_t> &reg_list );
-
 enum	output_type	{ RAW, MICRO_VOLT };
 
 using 	microvolt_t	= NAFE13388_UIM::microvolt_t;
 using 	raw_t		= NAFE13388_UIM::raw_t;
 
+using enum	NAFE13388_UIM::Register16;
+using enum	NAFE13388_UIM::Register24;
+using enum	NAFE13388_UIM::Command;
+
+void	DRDY_int_handler( void );
+void	logical_ch_config_view( void );
+void	table_view( int size, int cols, std::function<void(int)> view, std::function<void(void)> linefeed = nullptr );
+
+
 int main( void )
 {
 	printf( "***** Hello, NAFE13388 UIM board! *****\r\n" );
 
-	volatile auto	timeout_count	= timeout_limit;
+	auto	timeout_count	= timeout_limit;
 
-	spi.frequency( 1000 * 1000 );
+	spi.frequency( 1000'000 );
 	spi.mode( 1 );
 
 	afe.begin();
 
-	afe.logical_ch_config( 0, 0x1070, 0x0084, 0x2900, 0x0000 );
-	afe.logical_ch_config( 1, 0x2070, 0x0084, 0x2900, 0x0000 );
+	afe.logical_ch_config( 0, 0x1070, 0x3084, 0x2900, 0x0000 );
+	afe.logical_ch_config( 1, 0x2070, 0x3084, 0x2900, 0x0000 );
 
-	logical_ch_config_view( 0 );
-	logical_ch_config_view( 1 );
+	printf( "\r\nenabled logical channel(s) %2d\r\n", afe.enabled_channels );
+	logical_ch_config_view();
 
 	ADC_nDRDY.rise( DRDY_int_handler );
 
@@ -44,9 +55,9 @@ int main( void )
 //	constexpr bool output_type_selection	= RAW;
 
 	if ( output_type_selection == MICRO_VOLT )
-		printf( "values in micro-volt\r\n" );
+		printf( "\r\nvalues in micro-volt\r\n" );
 	else
-		printf( "values in raw\r\n" );
+		printf( "\r\nvalues in raw\r\n" );
 
 
 	while ( true )
@@ -82,22 +93,51 @@ void DRDY_int_handler( void )
 	drdy_wait	= false;
 }
 
-void logical_ch_config_view( int channel )
+void logical_ch_config_view( void )
 {
-	printf( "logical channel %02d\r\n", channel );
-	afe.write_r16( channel );
+	uint16_t en_ch_bitmap	= afe.reg( CH_CONFIG4 );
+	
+	for ( auto channel = 0; channel < 16; channel++ )
+	{	
+		printf( "  logical channel %2d : ", channel );
 
-	std::vector<uint16_t>	reg_list = { 0x0020, 0x0021, 0x0022, 0x0023 };
-	register16_dump( reg_list );
-
-	printf( "\r\n" );
+		if ( en_ch_bitmap & (0x1 << channel) )
+		{
+			afe.command( channel );
+			table_view( 4, 4, []( int v ){ printf( "  0x%04X: 0x%04X", (uint16_t)(v + CH_CONFIG0), afe.reg( v + CH_CONFIG0 ) ); } );
+		}
+		else
+		{
+			printf(  "  (disabled)\r\n" );
+		}
+	}
 }
 
-void register16_dump( const std::vector<uint16_t> &reg_list )
+void table_view( int length, int cols, std::function<void(int)> value, std::function<void(void)> linefeed )
 {
-	for_each(
-		reg_list.begin(),
-		reg_list.end(),
-		[]( auto reg ) { printf( "  0x%04X: 0x%04X\r\n", reg, afe.read_r16( reg ) ); }
-	);
+	const auto	raws	= (int)(length + cols - 1) / cols;
+	
+	for ( auto i = 0; i < raws; i++  )
+	{
+		if ( i )
+		{
+			if ( linefeed )
+				linefeed();
+			else
+				printf( "\r\n" );
+		}
+		
+		for ( auto j = 0; j < cols; j++  )
+		{
+			auto	index	= i + j * raws;
+			
+			if ( index < length  )
+				value( index );
+		}
+	}
+	
+	if ( linefeed )
+		linefeed();
+	else
+		printf( "\r\n" );
 }
