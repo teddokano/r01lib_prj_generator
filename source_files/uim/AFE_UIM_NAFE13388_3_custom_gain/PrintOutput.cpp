5ff471ab5cb171b1fe3d9c5a3f45380315b090d8
Tedd OKANO <okano.akifumi@gmail.com>
2025-03-11 09:14:27 +0900

    "PrintOutput" class updated to use colors by keywords

--- source_files/uim/AFE_UIM_NAFE13388_3_custom_gain/PrintOutput.cpp
+++ source_files/uim/AFE_UIM_NAFE13388_3_custom_gain/PrintOutput.cpp
@@ -63,7 +63,47 @@ void PrintOutput::printf( const char *format, ... )
 		fputs( s, fp );
 }
 
-void PrintOutput::screen( const char* s )
+void PrintOutput::screen( const char* s, bool file_output )
 {
-	::printf( "%s", s );
+	if ( file_output )
+		printf( "%s", s );		
+	else
+		::printf( "%s", s );
 }
+
+void PrintOutput::color( Color col, bool file_output )
+{
+	uint8_t		code;
+	char		s[ 10 ];
+	char		*sp	= s;
+	const char	*cfp;
+	char		c;
+	
+	for ( auto i = 0; i < 3; i++ )
+	{
+		code	= (static_cast<uint16_t>( col ) >> (i * 4)) & 0xF;
+
+		if ( !code )
+			continue;
+		
+		if ( code == 0x9 )
+			code	= (i == 2) ? 0 : 9;
+		else
+			--code;
+		
+		code	+= 0x30;
+		
+		cfp	= code_format[ i ];
+		
+		while ( (c	= *cfp++) )
+		{
+			*sp++	= (c == '*') ? code : c;
+		}
+		*sp	= 0;
+		
+		screen( s );
+	}
+}
+
+const char*	PrintOutput::code_format[] = { "\033[3*m", "\033[4*m", "\033[*m" };
+
--- source_files/uim/AFE_UIM_NAFE13388_3_custom_gain/PrintOutput.h
+++ source_files/uim/AFE_UIM_NAFE13388_3_custom_gain/PrintOutput.h
@@ -8,15 +8,93 @@
 #define NAFE_PRINTOUTPUT_H
 
 #include	<stdio.h>
+#include	<inttypes.h>
+
+/** PrintOutput class
+ *	
+ *  @class PrintOutput
+ *
+ *	PrintOutput is a class to support output text to screen and file. 
+ *	This supports color and modifier control by color() method.
+ */
 
 class	PrintOutput
 {
 public:
-	PrintOutput( const char *file_name, const char *file_ext = "csv", bool time_info = true );
+	
+	/** Color identifiers */
+	enum class Color : uint16_t {
+		black		= 0x001,
+		red			= 0x002,
+		green		= 0x003,
+		yellow		= 0x004,
+		blue		= 0x005,
+		magenta		= 0x006,
+		cyan		= 0x007,
+		gray		= 0x008,
+		none		= 0x009,
+		bg_black	= 0x010,
+		bg_red		= 0x020,
+		bg_green	= 0x030,
+		bg_yellow	= 0x040,
+		bg_blue		= 0x050,
+		bg_magenta	= 0x060,
+		bg_cyan		= 0x070,
+		bg_gray		= 0x080,
+		bg_none		= 0x090,
+		bold		= 0x200,
+		italic		= 0x400,
+		underline	= 0x500,
+		blink		= 0x600,
+		swap		= 0x800,
+		normal		= 0x900,
+	};
+	
+	/** Create a PrintOutput instance
+	 *
+	 * @param file_name default is nullptr to disabling file out
+	 * @param file_ext file name extention
+	 * @param time_info "true" for putinf date and time information in the file name
+	 */
+	PrintOutput( const char *file_name = nullptr, const char *file_ext = "csv", bool time_info = true );
+
+	/** Printf as normal printf
+	 */
 	void	printf( const char *format, ... );
-	void	screen( const char *s );
+
+	/** Screen output : Text output for screen only
+	 *
+	 * @param s string to output
+	 * @param file_output true, if file output is needed
+	 */
+	void	screen( const char *s, bool file_output = false );
+
+	/** Screen output : Text output for screen only
+	 *
+	 * @param c color and/or midifier
+	 * @param file_output true, if file output is needed
+	 */
+	void	color( Color c, bool file_output = false );
+
 private:
+	friend Color operator+( const Color& lhs, const Color& rhs )
+	{
+		return Color( static_cast<uint16_t>( lhs ) + static_cast<uint16_t>( rhs ) );
+	}
+
+	friend Color operator+( const Color& lhs, const int n )
+	{
+		return Color( static_cast<uint16_t>( lhs ) + n );
+	}
+
+	friend Color operator+( const int n, const Color& rhs )
+	{
+		return Color( n + static_cast<uint16_t>( rhs ) );
+	}
+
 	FILE	*fp;
+	
+	static const char	*code_format[];
 };
 
 #endif	//	NAFE_PRINTOUTPUT_H
