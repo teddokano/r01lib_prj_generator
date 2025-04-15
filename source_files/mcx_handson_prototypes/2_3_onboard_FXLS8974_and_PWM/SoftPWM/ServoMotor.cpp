bf780686361f1e0c6fcdc6b56e00bcc576784fb3
Tedd OKANO <okano.akifumi@gmail.com>
2025-04-15 15:39:51 +0900

    new sample (accelerometer+PWM) is added to mcx_handson

---
+++ source_files/mcx_handson_prototypes/2_3_onboard_FXLS8974_and_PWM/SoftPWM/ServoMotor.cpp
@@ -0,0 +1,43 @@
+//FILEHEAD
+#include	"ServoMotor.h"
+
+ServoMotor::ServoMotor( int pin_name, float pulse_width_min, float pulse_width_max, float angle_value_min, float angle_value_max )
+	:	SoftPWM_base( pin_name, 50.0, 1000, true, 0.1 ), 
+		min_pulse( pulse_width_min / (1.0 / freq) ),
+		range_pulse( pulse_width_max  / (1.0 / freq) - min_pulse ),
+		min_angle( angle_value_min ), 
+		range_angle( angle_value_max - angle_value_min )
+{
+}
+
+float ServoMotor::angle( float degree )
+{
+	deg	= degree;
+	
+	float	pulse_width	= ((degree - min_angle) / range_angle) * range_pulse + min_pulse;
+	duty( pulse_width );
+	
+	return deg;
+}
+
+float ServoMotor::angle( void )
+{
+	return deg;
+}
+
+void ServoMotor::range( float min, float max )
+{
+	min_angle	= min;
+	range_angle	= max - min;
+}
+
+ServoMotor& ServoMotor::operator=( float a )
+{
+	angle( a );
+	return *this;
+}
+
+ServoMotor& ServoMotor::operator=( ServoMotor& )
+{
+	return *this;
+}
---
+++ source_files/mcx_handson_prototypes/2_3_onboard_FXLS8974_and_PWM/SoftPWM/SoftPWM.cpp
@@ -0,0 +1,18 @@
+//FILEHEAD
+#include	"SoftPWM.h"
+
+SoftPWM::SoftPWM( int pin_name )
+	: SoftPWM_base( pin_name, 1.0, 10, false, 0.1 )
+{
+}
+
+SoftPWM& SoftPWM::operator=( float d )
+{
+	duty( d );
+	return *this;
+}
+
+SoftPWM& SoftPWM::operator=( SoftPWM& )
+{
+	return *this;
+}
