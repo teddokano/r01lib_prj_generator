72c5ee6fea5fa41547cd9c429e7b49f3f4abf774
Tedd OKANO <okano.akifumi@gmail.com>
2025-08-24 08:37:49 +0900

    clean script to handle VSC project

--- script/clean.py
+++ script/clean.py
@@ -91,7 +91,7 @@ for target in prjs_path:
 			else:
 				subprocess.run( "rm -rf " + cnfg + "/", shell = True )
 					
-		subprocess.run( "rm *.launch *.mex", shell = True )
+		subprocess.run( "rm -rf *.launch *.mex CMakeLists.txt flags.cmake CMakePresets.json mcux_include.json user_override.cmake .vscode/", shell = True )
 
 print( "" )
 print( "======= process completed for .. =======" )
